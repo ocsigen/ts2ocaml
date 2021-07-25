339ba2736367fad42d7f59b0c036c9054a418196
cannorin <cannorin@users.noreply.github.com>
2021-07-25 17:03:54 +0900

    Omit label if the function has only one non-optional argument
    Close #11

--- src/Targets/JsOfOCaml/Writer.fs
+++ src/Targets/JsOfOCaml/Writer.fs
@@ -644,6 +644,8 @@ let rec emitTypeImpl (flags: EmitTypeFlags) (overrideFunc: OverrideFunc) (ctx: C
           acc + tprintf "%s:" (Naming.valueName x.name |> rename) + between "(" ")" (emitTypeImpl { flags with forceVariadic = true } overrideFunc ctx x.value +@ " [@js.variadic]")
         | Choice2Of2 t :: [] | Choice1Of2 { value = t } :: [] when f.isVariadic ->
           acc + between "(" ")" (emitTypeImpl { flags with forceVariadic = true } overrideFunc ctx t +@ " [@js.variadic]")
+        | Choice1Of2 x :: [] when not x.isOptional ->
+          go acc [Choice2Of2 x.value] // do not generate label if it is the only argument and is not optional
         | Choice1Of2 x :: xs ->
           let prefix =
             if x.isOptional then "?" else ""

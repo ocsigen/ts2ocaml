708cd745cc758120b3c9786250a8b14dabfcdf7b
cannorin <cannorin@users.noreply.github.com>
2021-10-12 19:42:09 +0900

    Simplify test

--- test/src/main.ml
+++ test/src/main.ml
@@ -11,28 +11,9 @@
 open Ts2ocaml
 module TypeScript = Typescript.Export
 module Yargs = Yargs.Export
-
-module CSS = Csstype.Export
-module PropTypes = Prop_types.Export
-module SchedulerInteraction = Scheduler__tracing.Export.Interaction
 module React = React.Export
 module React_modal = React_modal.Export
 
 let () =
-  Printf.printf "%f\n" (Math.pi ());
-  Printf.printf "%f\n" (Math.cos (Math.pi () /. 2.0));
-  Printf.printf "%f\n" (Number.parseInt ~string:"42" ());
-  let s = String.bold (String.of_ml "foo") in
-
-  let result = String.match_ (String.of_ml "cabaret") ~regexp:(`String "[abc]+") in
-  let () =
-    begin match result with
-    | Some xs ->
-      let ys = Array.sort xs () in
-      Dom.Console.log (Dom.console ()) ~data:[Obj.magic ys];
-    | None -> ()
-    end
-  in
-  Printf.printf "typescript version: %s\n" (TypeScript.version ());
-  ()
+  Printf.printf "typescript version: %s\n" (TypeScript.version ())
 ;;
